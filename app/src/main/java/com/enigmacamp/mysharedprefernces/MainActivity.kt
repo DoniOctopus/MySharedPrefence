7f76237327ae475f95d4c94514ca1fcd8c361321
doni <donioctavian1016@gmail.com>
2021-03-05 14:44:20 +0700

    camera

--- app/src/main/java/com/enigmacamp/mysharedprefernces/MainActivity.kt
+++ app/src/main/java/com/enigmacamp/mysharedprefernces/MainActivity.kt
@@ -3,51 +3,62 @@ package com.enigmacamp.mysharedprefernces
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(),View.OnClickListener {
 
-    val REQUEST_CODE = 777
+    val BROWSE_REQUEST_CODE = 777
+    private val CAMERA_REQUEST_CODE = 1888
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         askPermission()
         button_browse.setOnClickListener(this)
+        button_camera.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent, REQUEST_CODE)
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val browserIntent = Intent(Intent.ACTION_PICK)
+        browserIntent.type = "image/*"
+        if (v == button_browse){
+            startActivityForResult(browserIntent, BROWSE_REQUEST_CODE)
+        }else{
+            startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
+        }
     }
 
     //isi dari ini adalah menagkap balikan dari activtyForResultnya
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode.equals(REQUEST_CODE)){
+        Log.i("INI HARUS DIPANGIL",requestCode.toString())
+        //untuk menampilkan allow sebagai popup
+        if (requestCode.equals(BROWSE_REQUEST_CODE)){
             val resultURI = data?.data
             //buat nampilih datanya masuk atau tidak
             Log.i("BROWSE_IMAGE_RESULT",resultURI.toString())
             imageView.setImageURI(resultURI)
         }
-    }
+        if(requestCode.equals(CAMERA_REQUEST_CODE)){
+            val photo: Bitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(photo)
+             }
+         }
 
     fun askPermission(){
+        //mencek apakah sudah pernah di allow atau tidak
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!=PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA),9090)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA),CAMERA_REQUEST_CODE)
             return
         }
         Log.i("PERMISSION","SELESAI")
     }
-
 }
